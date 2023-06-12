bf0243b2819fd8c30ccfcb7e827e4483d3f0fcd5
yening <yening@chengduoduo.net>
2023-06-12 18:20:33 +0800

    心跳获取jenkins构建状态

--- src/views/tree/index.vue
+++ src/views/tree/index.vue
@@ -1,249 +1,48 @@
-<!-- <template>
-  <div class="app-container">
-    <el-input v-model="filterText" placeholder="Filter keyword" style="margin-bottom:30px;" />
-
-    <el-tree
-      ref="tree2"
-      :data="data2"
-      :props="defaultProps"
-      :filter-node-method="filterNode"
-      class="filter-tree"
-      default-expand-all
-    />
-
-  </div>
-</template>
-
-<script>
-export default {
-
-  data() {
-    return {
-      filterText: '',
-      data2: [{
-        id: 1,
-        label: 'Level one 1',
-        children: [{
-          id: 4,
-          label: 'Level two 1-1',
-          children: [{
-            id: 9,
-            label: 'Level three 1-1-1'
-          }, {
-            id: 10,
-            label: 'Level three 1-1-2'
-          }]
-        }]
-      }, {
-        id: 2,
-        label: 'Level one 2',
-        children: [{
-          id: 5,
-          label: 'Level two 2-1'
-        }, {
-          id: 6,
-          label: 'Level two 2-2'
-        }]
-      }, {
-        id: 3,
-        label: 'Level one 3',
-        children: [{
-          id: 7,
-          label: 'Level two 3-1'
-        }, {
-          id: 8,
-          label: 'Level two 3-2'
-        }]
-      }],
-      defaultProps: {
-        children: 'children',
-        label: 'label'
-      }
-    }
-  },
-  watch: {
-    filterText(val) {
-      this.$refs.tree2.filter(val)
-    }
-  },
-
-  methods: {
-    filterNode(value, data) {
-      if (!value) return true
-      return data.label.indexOf(value) !== -1
-    }
-  }
-}
-</script>
- -->
-<!--
+js
 <template>
-  <div class="page-content">
-    <el-form ref="form" :model="form" :rules="rules" label-width="120px" style="max-width:600px;margin:0 auto;">
-      <el-form-item label="名称" prop="name">
-        <el-input v-model="form.name" />
-      </el-form-item>
-      <el-form-item label="描述" prop="description">
-        <el-input v-model="form.description" />
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="submitForm('form')">提交</el-button>
-      </el-form-item>
-    </el-form>
-  </div>
+  <div>请求状态：{{ requestStatus }}</div>
 </template>
 
 <script>
-
-import { testPostData } from '@/api/table'
-// import Axios from 'axios'
+import axios from 'axios'
 
 export default {
   data() {
     return {
-      form: {
-        name: '',
-        description: ''
-      },
-      rules: {
-        name: [{ required: true, message: '请输入名称', trigger: 'blur' }],
-        description: [{ required: true, message: '请输入描述', trigger: 'blur' }]
-      }
+      requestStatus: '未请求',
+      intervalId: null,
+      counter: 0,
+      maxCount: 600, // 最多请求600次，即10分钟
+      apiUrl: 'http://127.0.0.1:8080/devops/jenkins/job-status?job-name=flight_uat'
     }
   },
+  mounted() {
+    this.intervalId = setInterval(() => {
+      this.makeRequest()
+    }, 100000)
+  },
   methods: {
-    submitForm(formName) {
-      this.$refs[formName].validate((valid) => {
-        if (valid) {
-          const postData = {
-            name: this.form.name,
-            description: this.form.description
+    makeRequest() {
+      axios
+        .get(this.apiUrl)
+        .then(response => {
+          if (response.data === true) {
+            console.log(response.data)
+            this.requestStatus = '请求成功'
+            clearInterval(this.intervalId)
+          } else {
+            this.counter++
+            this.requestStatus = `正在请求中...（第 ${this.counter} 次）`
+            if (this.counter >= this.maxCount) {
+              clearInterval(this.intervalId)
+              this.requestStatus = '超时停止请求'
+            }
           }
-          testPostData(postData).then(response => {
-            // this.$message.success('提交成功')
-            console.log(response)
-          })
-          // Axios.post('/devops/api/postData', postData).then(response => {
-          //   this.$message.success('提交成功')
-          // })
-          // this.$http.post('/api/postData', postData).then((res) => {
-          //   this.$message.success('提交成功')
-          // })
-        } else {
-          return false
-        }
-      })
-    }
-  }
-}
-</script> -->
-<!-- <template>
-  <el-form ref="form" :model="form" :rules="rules" label-width="120px">
-    <el-form-item label="姓名" prop="name">
-      <el-input v-model="form.name" />
-    </el-form-item>
-
-    <el-form-item label="年龄" prop="age">
-      <el-input-number v-model="form.age" :min="1" :max="150" />
-    </el-form-item>
-
-    <el-form-item label="邮箱" prop="email">
-      <el-input v-model="form.email" />
-    </el-form-item>
-
-    <el-form-item label="手机号码" prop="phone">
-      <el-input v-model="form.phone" />
-    </el-form-item>
-
-    <el-form-item>
-      <el-button type="primary" @click="submitForm('form')">提交</el-button>
-      <el-button @click="resetForm('form')">重置</el-button>
-    </el-form-item>
-  </el-form>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      form: {
-        name: '',
-        age: '',
-        email: '',
-        phone: ''
-      },
-      rules: {
-        name: [
-          { required: true, message: '请输入姓名', trigger: 'blur' }
-        ],
-        age: [
-          { required: true, message: '请输入年龄', trigger: 'blur' },
-          { type: 'number', message: '必须是数字值', trigger: 'blur' },
-          { min: 1, max: 150, message: '年龄在1到150之间', trigger: 'blur' }
-        ],
-        email: [
-          { required: true, message: '请输入邮箱地址', trigger: 'blur' },
-          { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
-        ],
-        phone: [
-          { required: true, message: '请输入手机号码', trigger: 'blur' },
-          { pattern: /^1[34578]\d{9}$/, message: '请输入正确的手机号码', trigger: ['blur', 'change'] }
-        ]
-      }
-    }
-  },
-
-  methods: {
-    submitForm(formName) {
-      this.$refs[formName].validate(valid => {
-        if (valid) {
-          alert('表单校验通过')
-        } else {
-          alert('请检查表单填写内容')
-          return false
-        }
-      })
-    },
-
-    resetForm(formName) {
-      this.$refs[formName].resetFields()
+        })
+        .catch(error => {
+          console.error(error)
+        })
     }
   }
 }
 </script>
- -->
-<template>
-  <div>
-    <button :class="{'is-fetching': isFetching}" :disabled="isFetching" @click="fetchData">获取状态</button>
-  </div>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      isFetching: false
-    }
-  },
-  methods: {
-    fetchData() {
-      if (this.isFetching) return
-      this.isFetching = true
-      // 发起请求
-      // ...
-      console.log(window.localStorage.getItem('username'))
-      // 请求结束
-      setTimeout(() => {
-        this.isFetching = false
-      }, 3000)
-    }
-  }
-}
-</script>
-
-<style>
-button.is-fetching {
-  background-color: #ccc; /* 改变背景颜色 */
-  cursor: not-allowed; /* 禁用鼠标点击事件 */
-}
-</style>
