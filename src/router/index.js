485baa6cb9353881bc100929c35ed224b485ce1e
yening <yening@chengduoduo.net>
2023-06-19 08:46:49 +0800

    Token 过期清楚cookie

--- src/router/index.js
+++ src/router/index.js
@@ -99,187 +99,187 @@ export const constantRoutes = [
       }
     ]
   },
+  // 暂不显示
+  // {
+  //   path: '/kubernetes',
+  //   component: Layout,
+  //   redirect: '/kubernetes/table',
+  //   name: 'Kubernetes',
+  //   meta: { title: 'Kubernetes', icon: 'el-icon-cloudy' },
+  //   children: [
+  //     {
+  //       path: 'dev',
+  //       name: 'Dev',
+  //       component: () => import('@/views/kubernetes/dev/index'),
+  //       meta: { title: 'Dev', icon: 'el-icon-hot-water' }
+  //     },
+  //     {
+  //       path: 'fat',
+  //       name: 'Fat',
+  //       component: () => import('@/views/kubernetes/fat/index'),
+  //       meta: { title: 'Fat', icon: 'el-icon-ice-cream' }
+  //     },
+  //     // {
+  //     //   path: 'office',
+  //     //   name: 'Office',
+  //     //   component: () => import('@/views/kubernetes/office/index'),
+  //     //   meta: { title: 'Office', icon: 'el-icon-dessert' }
+  //     // },
+  //     {
+  //       path: 'deployment',
+  //       name: 'DeployMent',
+  //       component: () => import('@/views/kubernetes/deployment/index'),
+  //       meta: { title: '工作负载', icon: '' }
+  //     },
+  //     {
+  //       path: 'service',
+  //       name: 'Service',
+  //       component: () => import('@/views/kubernetes/deployment/index'),
+  //       meta: { title: '服务发现', icon: '' }
+  //     },
+  //     {
+  //       path: 'ingress',
+  //       name: 'Ingress',
+  //       component: () => import('@/views/kubernetes/deployment/index'),
+  //       meta: { title: '负载均衡', icon: '' }
+  //     },
+  //     {
+  //       path: 'cronjob',
+  //       name: 'CronJob',
+  //       component: () => import('@/views/kubernetes/deployment/index'),
+  //       meta: { title: '定时任务', icon: '' }
+  //     }
+  //     // {
+  //     //   path: 'prd',
+  //     //   name: 'Prd',
+  //     //   component: () => import('@/views/kubernetes/prd/index'),
+  //     //   meta: { title: 'Prd', icon: 'el-icon-dessert' }
+  //     // },
+  //     // {
+  //     //   path: 'old',
+  //     //   name: 'Old',
+  //     //   component: () => import('@/views/kubernetes/old/index'),
+  //     //   meta: { title: 'Old', icon: 'el-icon-dessert' }
+  //     // },
+  //     // {
+  //     //   path: 'struct',
+  //     //   name: 'Struct',
+  //     //   component: () => import('@/views/kubernetes/struct/index'),
+  //     //   meta: { title: 'Struct', icon: 'el-icon-dessert' }
+  //     // }
+  //   ]
+  // },
 
-  {
-    path: '/kubernetes',
-    component: Layout,
-    redirect: '/kubernetes/table',
-    name: 'Kubernetes',
-    meta: { title: 'Kubernetes', icon: 'el-icon-cloudy' },
-    children: [
-      {
-        path: 'dev',
-        name: 'Dev',
-        component: () => import('@/views/kubernetes/dev/index'),
-        meta: { title: 'Dev', icon: 'el-icon-hot-water' }
-      },
-      {
-        path: 'fat',
-        name: 'Fat',
-        component: () => import('@/views/kubernetes/fat/index'),
-        meta: { title: 'Fat', icon: 'el-icon-ice-cream' }
-      },
-      // {
-      //   path: 'office',
-      //   name: 'Office',
-      //   component: () => import('@/views/kubernetes/office/index'),
-      //   meta: { title: 'Office', icon: 'el-icon-dessert' }
-      // },
-      {
-        path: 'deployment',
-        name: 'DeployMent',
-        component: () => import('@/views/kubernetes/deployment/index'),
-        meta: { title: '工作负载', icon: '' }
-      },
-      {
-        path: 'service',
-        name: 'Service',
-        component: () => import('@/views/kubernetes/deployment/index'),
-        meta: { title: '服务发现', icon: '' }
-      },
-      {
-        path: 'ingress',
-        name: 'Ingress',
-        component: () => import('@/views/kubernetes/deployment/index'),
-        meta: { title: '负载均衡', icon: '' }
-      },
-      {
-        path: 'cronjob',
-        name: 'CronJob',
-        component: () => import('@/views/kubernetes/deployment/index'),
-        meta: { title: '定时任务', icon: '' }
-      }
-      // {
-      //   path: 'prd',
-      //   name: 'Prd',
-      //   component: () => import('@/views/kubernetes/prd/index'),
-      //   meta: { title: 'Prd', icon: 'el-icon-dessert' }
-      // },
-      // {
-      //   path: 'old',
-      //   name: 'Old',
-      //   component: () => import('@/views/kubernetes/old/index'),
-      //   meta: { title: 'Old', icon: 'el-icon-dessert' }
-      // },
-      // {
-      //   path: 'struct',
-      //   name: 'Struct',
-      //   component: () => import('@/views/kubernetes/struct/index'),
-      //   meta: { title: 'Struct', icon: 'el-icon-dessert' }
-      // }
-    ]
-  },
+  // {
+  //   path: '/example',
+  //   component: Layout,
+  //   redirect: '/example/table',
+  //   name: 'Example',
+  //   meta: { title: 'Example', icon: 'el-icon-s-help' },
+  //   children: [
+  //     {
+  //       path: 'table',
+  //       name: 'Table',
+  //       component: () => import('@/views/table/index'),
+  //       meta: { title: 'Table', icon: 'table' }
+  //     },
+  //     {
+  //       path: 'tree',
+  //       name: 'Tree',
+  //       component: () => import('@/views/tree/index'),
+  //       meta: { title: 'Tree', icon: 'tree' }
+  //     },
+  //     {
+  //       path: 'expand',
+  //       name: 'Expand',
+  //       component: () => import('@/views/expand/index'),
+  //       meta: { title: 'Expand', icon: 'el-icon-arrow-right' }
+  //     }
+  //   ]
+  // },
 
-  {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'el-icon-s-help' },
-    children: [
-      {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
-      },
-      {
-        path: 'expand',
-        name: 'Expand',
-        component: () => import('@/views/expand/index'),
-        meta: { title: 'Expand', icon: 'el-icon-arrow-right' }
-      }
-    ]
-  },
+  // {
+  //   path: '/form',
+  //   component: Layout,
+  //   children: [
+  //     {
+  //       path: 'index',
+  //       name: 'Form',
+  //       component: () => import('@/views/form/index'),
+  //       meta: { title: 'Form', icon: 'form' }
+  //     }
+  //   ]
+  // },
 
-  {
-    path: '/form',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        name: 'Form',
-        component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' }
-      }
-    ]
-  },
+  // {
+  //   path: '/nested',
+  //   component: Layout,
+  //   redirect: '/nested/menu1',
+  //   name: 'Nested',
+  //   meta: {
+  //     title: 'Nested',
+  //     icon: 'nested'
+  //   },
+  //   children: [
+  //     {
+  //       path: 'menu1',
+  //       component: () => import('@/views/nested/menu1/index'), // Parent router-view
+  //       name: 'Menu1',
+  //       meta: { title: 'Menu1' },
+  //       children: [
+  //         {
+  //           path: 'menu1-1',
+  //           component: () => import('@/views/nested/menu1/menu1-1'),
+  //           name: 'Menu1-1',
+  //           meta: { title: 'Menu1-1' }
+  //         },
+  //         {
+  //           path: 'menu1-2',
+  //           component: () => import('@/views/nested/menu1/menu1-2'),
+  //           name: 'Menu1-2',
+  //           meta: { title: 'Menu1-2' },
+  //           children: [
+  //             {
+  //               path: 'menu1-2-1',
+  //               component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
+  //               name: 'Menu1-2-1',
+  //               meta: { title: 'Menu1-2-1' }
+  //             },
+  //             {
+  //               path: 'menu1-2-2',
+  //               component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
+  //               name: 'Menu1-2-2',
+  //               meta: { title: 'Menu1-2-2' }
+  //             }
+  //           ]
+  //         },
+  //         {
+  //           path: 'menu1-3',
+  //           component: () => import('@/views/nested/menu1/menu1-3'),
+  //           name: 'Menu1-3',
+  //           meta: { title: 'Menu1-3' }
+  //         }
+  //       ]
+  //     },
+  //     {
+  //       path: 'menu2',
+  //       component: () => import('@/views/nested/menu2/index'),
+  //       name: 'Menu2',
+  //       meta: { title: 'menu2' }
+  //     }
+  //   ]
+  // },
 
-  {
-    path: '/nested',
-    component: Layout,
-    redirect: '/nested/menu1',
-    name: 'Nested',
-    meta: {
-      title: 'Nested',
-      icon: 'nested'
-    },
-    children: [
-      {
-        path: 'menu1',
-        component: () => import('@/views/nested/menu1/index'), // Parent router-view
-        name: 'Menu1',
-        meta: { title: 'Menu1' },
-        children: [
-          {
-            path: 'menu1-1',
-            component: () => import('@/views/nested/menu1/menu1-1'),
-            name: 'Menu1-1',
-            meta: { title: 'Menu1-1' }
-          },
-          {
-            path: 'menu1-2',
-            component: () => import('@/views/nested/menu1/menu1-2'),
-            name: 'Menu1-2',
-            meta: { title: 'Menu1-2' },
-            children: [
-              {
-                path: 'menu1-2-1',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-                name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1' }
-              },
-              {
-                path: 'menu1-2-2',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-                name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2' }
-              }
-            ]
-          },
-          {
-            path: 'menu1-3',
-            component: () => import('@/views/nested/menu1/menu1-3'),
-            name: 'Menu1-3',
-            meta: { title: 'Menu1-3' }
-          }
-        ]
-      },
-      {
-        path: 'menu2',
-        component: () => import('@/views/nested/menu2/index'),
-        name: 'Menu2',
-        meta: { title: 'menu2' }
-      }
-    ]
-  },
-
-  {
-    path: 'external-link',
-    component: Layout,
-    children: [
-      {
-        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-        meta: { title: 'External Link', icon: 'link' }
-      }
-    ]
-  },
+  // {
+  //   path: 'external-link',
+  //   component: Layout,
+  //   children: [
+  //     {
+  //       path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
+  //       meta: { title: 'External Link', icon: 'link' }
+  //     }
+  //   ]
+  // },
 
   // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true }
--- src/utils/request.js
+++ src/utils/request.js
@@ -59,6 +59,13 @@ service.interceptors.response.use(
     let customMessage = ''
     if (error.response.status === 401) { // 没有登录权限 Token 验证失败，退出登录并跳转到登录页面
       customMessage = 'Token 失效，请重新登录'
+      var cookies = document.cookie.split(';')
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i]
+        var eqPos = cookie.indexOf('=')
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie
+        document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT'
+      }
       document.cookie = 'vue_admin_template_token' + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/'
       router.push('/login')
     }
