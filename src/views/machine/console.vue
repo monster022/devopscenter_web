a3b244873ba029a3f9385fc1748f16c336629655
yening <yening@chengduoduo.net>
2023-06-21 18:08:05 +0800

    机器列表添加终端按钮

--- src/views/machine/console.vue
+++ src/views/machine/console.vue
@@ -12,13 +12,38 @@ export default {
   props: {
     hostname: {
       type: String,
-      default: 'default'
+      default: '[root@127.0.0.1 ~]$ '
     }
   },
   mounted() {
-    const term = new Terminal()
+    const term = new Terminal({
+      rows: 30,
+      cols: 80,
+      convertEol: true,
+      scrollback: 50,
+      disableStdin: false,
+      windowsMode: true,
+      cursorStyle: 'underline',
+      cursorBlink: true,
+      theme: {
+        foreground: '#000000',
+        background: '#FFFFF0',
+        cursor: 'help',
+        lineHeight: 20
+      }
+    })
     term.open(this.$refs.terminal)
-    term.write(this.hostname)
+
+    const socket = new WebSocket('ws://127.0.0.1:8080/devops/ws')
+    socket.onopen = () => {
+      term.writeln(this.hostname)
+    }
+    socket.onmessage = (event) => {
+      term.writeln(this.hostname + event.data)
+    }
+    term.onData((data) => {
+      socket.send(data)
+    })
   }
 }
 </script>
